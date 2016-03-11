9ef02792e10989fa511714a6d6ec2ff76ab3dc2f
Merlin <merlin@rebrovic.net>
2016-03-11 21:04:33 +0100

    Comment out GA for an experiment.

--- content/media/js/app.js
+++ content/media/js/app.js
@@ -1,3 +1,4 @@
+{# commenting out Google Analytics for an experiment
 {% if site.config.mode == "production" %}
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -7,6 +8,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', '{{ site.meta.ga_tracking_code }}', 'auto');
 ga('send', 'pageview');
 {% endif %}
+#}
 
 (function() {
     /**
